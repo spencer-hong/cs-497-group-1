9c507bbf0e3588cfd343334c1a4642b50b3ac470
Spencer Hong <spencerhong@u.northwestern.edu>
2022-02-07 13:27:26 -0600

    added  part of train

--- hw2/ffnn.py
+++ hw2/ffnn.py
@@ -3,9 +3,11 @@ from torch import nn
 import torch.nn.functional as F
 import torch.optim as optim
 import multiprocessing as mp
-
+from collections import defaultdict
+from datetime import datetime
 
 torch.manual_seed(100)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class FFNNParams():
 
@@ -13,13 +15,16 @@ class FFNNParams():
     self.embedding_dim = 100 # n x 100 embedding space
     self.hiddn_dim = 100 # an arbitrarily chosen dimension
     self.cpu_count = mp.cpu_count()
-    self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
     self.batch_size = 20
     self.epochs = 20
+    self.loss_func = nn.MSELoss()
+    self.optimizer = optim.Adam # pass this function later in train() with the model params
+    self.learning_rate = 1e-2
 
 
 class FFNN(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, context_size, hidden_dim):
+    def __init__(self, vocab_size, embedding_dim, context_size, hidden_dim, device = device):
         super(FFNN, self).__init__()
         self.context_size = context_size 
         self.embedding_dim = embedding_dim 
@@ -52,3 +57,26 @@ def prepare_dataset():
 def prepare_loader(dataset, batch_size, num_workers):
     return torch.utils.data.DataLoader(dataset, batch_size = batch_size, shuffle = True, num_workers = num_workers)
 
+
+
+def train(dataloader,  model, loss_func, optimizer):
+    model.train()
+
+    # perplexity, loss
+    values_to_track = defaultdict(list)
+
+    original_time = datetime.now()
+
+    for batch, (X, y) in enumerate(dataloader):
+
+        X, y = X.to(device), y.to(device)
+
+        pred = model(X)
+
+        loss = loss_func(pred, y.unsqueeze(1))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        
