eafb19b5072aa8edf8c77f1aec90f67e0641b541
hanklee97121 <71743877+hanklee97121@users.noreply.github.com>
2022-02-13 21:53:59 -0600

    Build RNN model

---
+++ hw2/RNN.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+
+class RNN_cell(nn.Module):
+  def __init__(self, input_size, hidden_size):
+    super(RNN_cell, self).__init__()
+    self.input_size = input_size
+    self.hidden_size = hidden_size
+    self.layer1 = nn.Linear(input_size+hidden_size, hidden_size)
+    self.layer2 = nn.Linear(hidden_size+hidden_size, hidden_size)
+  def forward(self, token, prev_hidden):
+    """
+    Args:
+      token: Tensor [batch_size, input_size]
+      prev_hidden: Tensor [2, batch_size, hidden_size] previous hidden state of each layer
+    Return:
+      output: Tensor [2, batch_size, hidden_size] hidden state of each layer
+    """
+    input = torch.cat((token, prev_hidden[0]), 1)
+    hidden1 = self.layer1(input)
+    input = torch.cat((hidden1, prev_hidden[1]), 1)
+    hidden2 = self.layer2(input)
+    output = torch.stack((hidden1, hidden2), dim=0)
+    return output
+
+class RNN(nn.Module):
+  def __init__(self, input_size, hidden_size, vocab_size):
+    super(RNN, self).__init__()
+    self.rnn_cell = RNN_cell(input_size, hidden_size)
+    self.output = nn.Sequential(
+        nn.Linear(hidden_size, vocab_size),
+        nn.Softmax()
+    )
+  def forward(self, sequence, init_hidden):
+    """
+    Args:
+      sequence: Tensor [sequence_length, batch_size, input_size]
+      init_hidden: Tensor [n_layer=2, batch_size, hidden_size]
+    Output:
+      Distribution: Tensor [vocab_size] assign probability to each word
+    """
+    for i in range(len(sequence)):
+      init_hidden = self.rnn_cell(sequence[i], init_hidden)
+    Distribution = self.output(init_hidden[1])#take hidden state of last layer
+
+    return Distribution
